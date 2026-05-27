a19009fdcbb0ca15384d24ff60ae4432227ab946
ElMatsiii <maximosazo@gmail.com>
2026-05-27 19:44:49 -0400

    cosas

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\maxim\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\ucn\tongoy_app (1)\tongoy_app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\maxim\OneDrive\Documentos\GitHub\Proyecto-Integrador-De-Plataformas"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\maxim\OneDrive\Documentos\GitHub\Proyecto-Integrador-De-Plataformas\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
